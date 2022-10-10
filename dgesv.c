7afb333b24beb1a1ee3e8519cd8a4083a513dc74
Koushik Chowdhury <45112318+chykoushik@users.noreply.github.com>
2022-10-10 21:53:04 +0200

    Update dgesv.c

--- dgesv.c
+++ dgesv.c
@@ -39,7 +39,7 @@ int check_result(double *bref, double *b, int size)
   return 1;
 }
 
-int my_dgesv(int n, int i, int j, int k,  float ratio, double *a, double *b)
+int my_dgesv(int n, int i, int j, int k, double mat, double *a, double *b)
 {
 
   //Replace next line to use your own DGESV implementation
@@ -49,22 +49,21 @@ int my_dgesv(int n, int i, int j, int k,  float ratio, double *a, double *b)
 	 {
 		  if(a[i] == 0.0)
 		  {
-			   //printf("Error!");
 			   exit(0);
 		  }
 		  for(j=i+1;j<=n;j++)
 		  {
-			   ratio = a[j]/a[i];
+			   mat  = a[j]/a[i];
 			   
 			   for(k=1;k<=n+1;k++)
 			   {
-			  		a[k] = a[k] - ratio*a[i];
+			  		a[k]=a[k] - mat*a[i];
+					b[j]=b[j] - mat*a[i];
 			   }
 		  }
 	 }
 	
-	 return(0);
-	}
+}
 
 void main(int argc, char *argv[])
 {
@@ -81,7 +80,7 @@ void main(int argc, char *argv[])
   // Using LAPACK dgesv OpenBLAS implementation to solve the system
   int n = size, nrhs = size, lda = size, ldb = size, info;
   int *ipiv = (int *) malloc(sizeof(int) * size);
-  int i, j, k; double ratio;
+  int i, j, k; double mat;
   clock_t tStart = clock();
   info = LAPACKE_dgesv(LAPACK_ROW_MAJOR, n, nrhs, aref, lda, ipiv, bref, ldb);
  printf("Time taken by OpenBLAS LAPACK: %.2fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
@@ -89,7 +88,7 @@ void main(int argc, char *argv[])
   int *ipiv2 = (int *) malloc(sizeof(int) * size);
 
   tStart = clock();
-  my_dgesv(n, i, j, k, ratio, a, b);
+  my_dgesv(n, i, j, k, mat, a, b);
   printf("Time taken by my implementation: %.2fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
 
   if (check_result(bref, b, size) == 1)
